5a377e1919cc03942db211f0498c390ccfdeffea
Liam Dawson <liam@ldaws.com>
2018-12-14 01:02:52 +1100

    support ssh keys for sudo?

--- administration.nix
+++ administration.nix
@@ -5,8 +5,13 @@
   services.openssh.permitRootLogin = "no";
   networking.firewall.allowedTCPPorts = [ 22 ];
 
+  environment.systemPackages = with pkgs;
+    [ pam_ssh_agent_auth ];
+
   # sudo via SSH keys? Yes please!
-  security.pam.enableSSHAgentAuth = true;
+  security.sudo.extraconfig = ''
+    auth sufficient ${pkgs.pam_ssh_agent_auth}/libexec/pam_ssh_agent_auth.so file=/etc/ssh/authorized_keys.d/%u"}
+  '';
 
   users.mutableUsers = false;
 
