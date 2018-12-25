9965897dcfe9d148db7fad7acd80b6c2de5e8a56
Liam Dawson <liam@ldaws.com>
2018-12-26 03:26:46 +1100

    missing semicolon

--- pihole.nix
+++ pihole.nix
@@ -5,5 +5,5 @@
     finalImageTag = "4.1";
     imageDigest = "sha256:3c165a8656d22b75ad237d86ba3bdf0d121088c144c0b2d34a0775a9db2048d7";
     sha256 = "132n8dz1k4k759fm5dzfb74218rv84wzn24kjwfl8fwmc6h28fsf";
-  }
+  };
 }
