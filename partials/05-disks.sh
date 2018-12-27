0ceb18b27f7e04f83dc431228d07def63b48c072
Liam Dawson <liam@ldaws.com>
2018-12-27 16:42:08 +1100

    external disk is btrfs

--- partials/05-disks.sh
+++ partials/05-disks.sh
@@ -12,6 +12,6 @@
 
   if ! sudo grep -qe 'PBX_MEDIA' /etc/fstab >/dev/null
   then
-    echo "/dev/disk/by-partlabel/PBX_MEDIA /media/data    ext4    defaults       0 0" | sudo tee -a /etc/fstab >/dev/null
+    echo "/dev/disk/by-partlabel/PBX_MEDIA /media/data    btrfs   defaults       0 0" | sudo tee -a /etc/fstab >/dev/null
   fi
 )
