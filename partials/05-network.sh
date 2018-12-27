b8577dbac2dbd86d299641c7391669f38821abed
Liam Dawson <liam@ldaws.com>
2018-12-27 15:13:41 +1100

    start configuring firewall

--- partials/05-network.sh
+++ partials/05-network.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
+echo ' - setting hostname...'
+
 echo 'plexbox.localdomain' | sudo tee /etc/hostname >/dev/null
 sudo hostname -F /etc/hostname
+
+declare -a FIREWALL_RULES=(
+  "firewall-cmd --set-default-zone=home"
+)
+
+echo ' - configuring firewall transiently...'
+for rule in "${FIREWALL_RULES[@]}"
+do
+  sudo "$rule"
+done
+
+echo ' - configuring firewall permanently...'
+for rule in "${FIREWALL_RULES[@]}"
+do
+  sudo "$rule" --permanent
+done
