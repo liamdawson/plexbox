ba3b5410076eec4bc483755b3aa40275105374e3
Liam Dawson <liam@ldaws.com>
2018-12-27 15:16:19 +1100

    fall back to shellcheck disable

--- partials/05-network.sh
+++ partials/05-network.sh
@@ -6,17 +6,19 @@ echo 'plexbox.localdomain' | sudo tee /etc/hostname >/dev/null
 sudo hostname -F /etc/hostname
 
 declare -a FIREWALL_RULES=(
-  (firewall-cmd --set-default-zone=home)
+  "firewall-cmd --set-default-zone=home"
 )
 
 echo ' - configuring firewall transiently...'
 for rule in "${FIREWALL_RULES[@]}"
 do
-  sudo "${rule[@]}"
+  # shellcheck disable=SC2086
+  sudo $rule
 done
 
 echo ' - configuring firewall permanently...'
 for rule in "${FIREWALL_RULES[@]}"
 do
-  sudo "${rule[@]}" --permanent
+  # shellcheck disable=SC2086
+  sudo $rule --permanent
 done
