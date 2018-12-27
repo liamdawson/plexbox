ada70f62e82790dd8ae9c71d6d181a1ea3a5c5bc
Liam Dawson <liam@ldaws.com>
2018-12-27 17:05:51 +1100

    try chcon instead of semanage

--- partials/20-sharing.sh
+++ partials/20-sharing.sh
@@ -6,6 +6,6 @@
 
   sudo yum install -y samba samba-client samba-common
 
-  sudo semanage fcontext -a -t samba_share_t '/media/data(/.*)?'
+  sudo chcon -t samba_share_t /media/data/
   sudo restorecon -R /media/data
 )
