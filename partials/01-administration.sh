a4fae186d1dd13f9069241213e2b4a0349d60929
Liam Dawson <liam@ldaws.com>
2018-12-27 16:09:51 +1100

    enable sudo via ssh keys

--- partials/01-administration.sh
+++ partials/01-administration.sh
@@ -4,5 +4,12 @@
   set -e
   set -u
 
+  echo " - enabling sudo via SSH keys"
   sudo yum install -y pam_ssh_agent_auth
+
+  #shellcheck disable=SC2024
+  sudo tee /etc/pam.d/sudo >/dev/null <"${_BASE_CONFIG_DIR}/files/sudo-pam"
+
+  #shellcheck disable=SC2024
+  sudo tee /etc/sudoers.d/01-ssh-sock-env >/dev/null <"${_BASE_CONFIG_DIR}/files/sudoers-ssh-sock"
 )
