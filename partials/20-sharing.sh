dc24cf97324ba844c0834b7f6e0d0589b2cf89c3
Liam Dawson <liam@ldaws.com>
2018-12-27 17:14:47 +1100

    samba config

--- partials/20-sharing.sh
+++ partials/20-sharing.sh
@@ -6,6 +6,13 @@
 
   sudo yum install -y samba samba-client samba-common
 
+  echo ' - setting SELinux permissions for shares'
   sudo chcon -t samba_share_t /media/data/
   sudo restorecon -R /media/data
+
+  echo ' - setting samba configuration'
+  #shellcheck disable=SC2024
+  sudo tee /etc/samba/smb.conf >/dev/null < "${_BASE_CONFIG_DIR}/files/smb.conf"
+
+  systemctl restart smb
 )
