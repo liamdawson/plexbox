7d00348ac335884498917d8a878afa6438fe5e4b
Liam Dawson <liam@ldaws.com>
2018-12-27 16:18:18 +1100

    install plex media server

---
+++ partials/20-media.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+(
+  set -e
+  set -u
+
+  PUBLIC_PLEX_MEDIA_SERVER_URL="https://downloads.plex.tv/plex-media-server/1.14.1.5488-cc260c476/plexmediaserver-1.14.1.5488-cc260c476.x86_64.rpm"
+
+  if ! yum list installed plex-media-server >/dev/null 2>&1
+  then
+    sudo yum install -y "$PUBLIC_PLEX_MEDIA_SERVER_URL"
+  fi
+)
