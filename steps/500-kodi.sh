f6e198a595fa27409cd2e3bd8ddb62bab7b39986
Liam Dawson <liam@ldaws.com>
2019-01-13 10:00:52 +0000

    user already exists fix

--- steps/500-kodi.sh
+++ steps/500-kodi.sh
@@ -5,7 +5,7 @@
 
   sudo apt install -y kodi kodi-bin
 
-  echo '' | sudo useradd kodi
+  id 'kodi' >/dev/null 2>&1 || echo '' | sudo useradd kodi
   sudo usermod -aG audio,video kodi
 
   sudo tee "/etc/systemd/system/kodi.service" >/dev/null <"${_BASE_CONFIG_DIR}/files/kodi.service"
