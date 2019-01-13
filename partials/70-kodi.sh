5c01b3e4dc7e4db7920b94a8997a38a4ff50c1da
Liam Dawson <liam@ldaws.com>
2019-01-13 19:37:48 +1100

    install x11

--- partials/70-kodi.sh
+++ partials/70-kodi.sh
@@ -4,6 +4,8 @@
   set -e
   set -u
 
+  sudo yum groupinstall -y x11
+
   if ! id 'kodi' >/dev/null 2>&1
   then
     echo '' | sudo useradd -p '$2a$07$AJ54EVMPMjzl7Dj9dGm0XuArAPaG3D/s6qZHjVmc2pdvxPHba9oty' kodi
