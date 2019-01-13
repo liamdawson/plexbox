9b2aa85a9b9c6c4261da517f4043784b2b9ba969
Liam Dawson <liam@ldaws.com>
2019-01-13 21:09:10 +1100

    install drivers

--- steps/200-gui.sh
+++ steps/200-gui.sh
@@ -3,8 +3,10 @@
 (
   set -eu
 
-  sudo apt install -y xorg xserver-xorg-legacy dbus-x11 alsa-utils usbmount
+  sudo apt install -y xorg xserver-xorg-legacy dbus-x11 alsa-utils usbmount ubuntu-drivers-common
 
   sudo tee "/etc/X11/Xwrapper.config" >/dev/null <"${_BASE_CONFIG_DIR}/files/Xwrapper.config"
   sudo tee "/etc/usbmount/usbmount.conf" >/dev/null <"${_BASE_CONFIG_DIR}/files/usbmount.conf"
+
+  sudo ubuntu-drivers autoinstall
 )
