d4ddc653cab269fddcb8d7d80775b6262808756e
Liam Dawson <liam@ldaws.com>
2019-01-15 15:53:52 +0000

    don't install kodi

--- steps/500-kodi.sh
+++ steps/500-kodi.sh
@@ -3,7 +3,8 @@
 (
   set -eu
 
-  sudo apt install -y kodi kodi-bin
+  # I think this has been causing DLNA/Sonos issues?
+  # sudo apt install -y kodi kodi-bin
 
   id 'kodi' >/dev/null 2>&1 || echo '' | sudo useradd -m kodi
   sudo usermod -aG audio,video kodi
